69cc21d26a990925923d00ac5208199dc8850877
Roman Vlasenko <klavionik@gmail.com>
2020-03-02 21:06:09 +0500

    Django solution 1 add

---
+++ lessons/django/solution1/app/views.py
@@ -0,0 +1,29 @@
+import os
+from datetime import datetime
+
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+
+
+def home_view(request):
+    template_name = 'app/home.html'
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    current_time = datetime.strftime(datetime.now(), '%X')
+    msg = f'Current server time: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    msg = f"Current working directory contains: {', '.join(os.listdir('.'))}"
+    return HttpResponse(msg)
