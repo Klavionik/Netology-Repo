4c616777d10dfed216642d62c9cba14e7093a0c2
Roman Vlasenko <klavionik@gmail.com>
2020-03-13 23:55:48 +0500

    Django solution 4 task 1 update, task 2 add

--- lessons/django/solution4/work_with_database/phones/models.py
+++ lessons/django/solution4/work_with_database/phones/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Phone(models.Model):
-    id = models.IntegerField(primary_key=True)
+
     name = models.CharField(max_length=64)
     image = models.TextField()
     price = models.IntegerField()
