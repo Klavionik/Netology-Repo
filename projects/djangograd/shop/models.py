82ebaa68e9267da177de5126f13b1a15f1def3a7
Roman Vlasenko <klavionik@gmail.com>
2020-04-09 14:11:44 +0500

    Django grad work: Initial commit

---
+++ projects/djangograd/shop/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Product(models.Model):
+    pass
+
+
+class Article(models.Model):
+    pass
+
+
+class Category(models.Model):
+    pass
+
+
+class Review(models.Model):
+    pass
